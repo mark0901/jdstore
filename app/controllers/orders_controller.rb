bc76c76a63cfc0b64d72dba741337a7ca7855ee1
markwang <markwang0901@gmail.com>
2017-05-24 20:01:47 +0800

    user can see their orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,9 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!, only: [:create]
+  before_action :authenticate_user!
+
+  def index
+    @orders = current_cart.orders
+  end
 
   def create
     @order = Order.new(order_params)
@@ -17,14 +21,14 @@ class OrdersController < ApplicationController
         product_list.save
       end
 
-      redirect_to order_path(@order)
+      redirect_to order_path(@order.token)
     else
       render 'carts/checkout'
     end
   end
 
   def show
-    @order = Order.find(params[:id])
+    @order = Order.find_by_token(params[:id])
     @product_lists = @order.product_lists
   end
 
