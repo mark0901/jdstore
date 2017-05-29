1c499df8875cbd01c2d1b8d7001cd55f1f527137
markwang <markwang0901@gmail.com>
2017-05-29 16:08:56 +0800

    config qiniu

--- Gemfile
+++ Gemfile
@@ -55,3 +55,6 @@ gem 'carrierwave'
 gem 'mini_magick'
 gem 'letter_opener', group: :development
 gem 'aasm'
+gem 'carrierwave-qiniu'
+gem 'qiniu-rs'
+gem 'figaro'
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     resources :products
     resources :orders do
       member do
-        post :cancel 
+        post :cancel
         post :ship
         post :shipped
         post :return
@@ -33,6 +33,7 @@ Rails.application.routes.draw do
     member do
       post :pay_with_alipay
       post :pay_with_wechat
+      post :apply_to_cancel
     end
   end
 
